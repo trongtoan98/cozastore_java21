0d9a1c755bc91c383c6ebf559a94f7952ae289de
huynhcuong <huynhcuong@gmail.com>
2023-08-12 22:05:54 +0700

    update product,category,size,color

--- src/main/java/com/cybersoft/cozastore_java21/controller/CategoryController.java
+++ src/main/java/com/cybersoft/cozastore_java21/controller/CategoryController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired
@@ -26,6 +28,7 @@ public class CategoryController {
     public ResponseEntity<?> getAllCategory(){
 
         BaseResponse response = new BaseResponse();
+        response.setStatusCode(200);
         response.setData(categoryServiceImp.getAllCategory());
 
         return new ResponseEntity<>(response, HttpStatus.OK);
--- src/main/java/com/cybersoft/cozastore_java21/payload/response/ProductResponse.java
+++ src/main/java/com/cybersoft/cozastore_java21/payload/response/ProductResponse.java
@@ -4,7 +4,17 @@ public class ProductResponse {
     private int id;
     private String name;
     private double price;
+    private String description;
     private String image;
+    private String imageDetail;
+
+    private SizeResponse size;
+
+    private ColorResponse color;
+
+    private CategoryResponse category;
+
+    private int quantity;
 
     public int getId() {
         return id;
@@ -30,11 +40,59 @@ public class ProductResponse {
         this.price = price;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getImage() {
         return image;
     }
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public SizeResponse getSize() {
+        return size;
+    }
+
+    public void setSize(SizeResponse size) {
+        this.size = size;
+    }
+
+    public ColorResponse getColor() {
+        return color;
+    }
+
+    public void setColor(ColorResponse color) {
+        this.color = color;
+    }
+
+    public CategoryResponse getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryResponse category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageDetail() {
+        return imageDetail;
+    }
+
+    public void setImageDetail(String imageDetail) {
+        this.imageDetail = imageDetail;
     }
 }
